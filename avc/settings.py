f73882edfd1565b1c6c5e321da90759df77b948d
unknown <service.tek.bot@mail.ru>
2022-12-03 16:10:17 +0600

    First commit

--- avc/settings.py
+++ avc/settings.py
@@ -154,9 +154,9 @@ LANGUAGES = (
 #    os.path.join(BASE_DIR, "static"),
 #]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
